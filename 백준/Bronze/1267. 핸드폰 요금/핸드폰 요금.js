21d76da8594f527d0248ac05ec3f31bab9c214ea
hoyaa <86460176+hoyyat@users.noreply.github.com>
2023-05-20 18:27:37 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.js
@@ -0,0 +1,22 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const N = input[0] 
+const arrTime = input[1].split(" ")
+let YSFee = 0;
+let MSFee = 0;
+
+for(let i = 0; i < N; i++) {
+    YSFee += 10 + (10 * parseInt(arrTime[i]/30))
+}
+
+for(let i = 0; i < N; i++) {
+    MSFee += 15 + (15 * parseInt(arrTime[i]/60))
+}
+
+if(YSFee < MSFee) {
+    console.log("Y",YSFee)
+} else if (YSFee === MSFee) {
+    console.log("Y M",YSFee)
+} else {
+    console.log("M",MSFee)
+}
