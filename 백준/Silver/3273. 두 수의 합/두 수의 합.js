775ff8d2547c03d725e3a2905d5eef7a90a128c2
hoyaa <86460176+hoyyat@users.noreply.github.com>
2023-05-22 19:29:33 +0900

    [Silver III] Title: 두 수의 합, Time: 224 ms, Memory: 18676 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.js
@@ -0,0 +1,23 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const columnLength = input[0]
+const column = input[1].split(" ").map(v => +v).sort((a, b) => a - b)
+const sum = +input[2]
+
+let start = 0;
+let end = columnLength - 1
+let cnt = 0;
+
+
+while (start != end) {
+    if (column[start] + column[end] === sum) {
+        cnt++;
+        start++;
+    } else if (column[start] + column[end] > sum) {
+        end--;
+    } else {
+        start++;
+    }
+}
+
+console.log(cnt)
