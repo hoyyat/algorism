7f48a05c93eaddc2f586554658b819b6a8d2f1f6
JINHO KWACK <86460176+hoyyat@users.noreply.github.com>
2024-04-02 12:55:05 +0900

    [Silver I] Title: 히오스 프로게이머, Time: 244 ms, Memory: 35364 KB -BaekjoonHub

--- 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.py
+++ 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.py
@@ -1,26 +1,30 @@
+# 백준 16564번 히오스 프로게이머
 import sys
 input = sys.stdin.readline
 
-n, k = map(int,input().split())
-levels = [ int(input()) for _ in range(n)]
+def game(levels):
+    start = levels[0] + 1 # 목표레벨의 최소값
+    end = levels[0] + K # 최대값
 
-start = min(levels)
-end = start + k
+    while (start <= end):
+        mid = (start + end) // 2
+        total = 0
+        for i in range(len(levels)):
+            if levels[i] < mid:
+                total += mid - levels[i]
+        if total <= K:
+            global answer
+            start = mid + 1
+            answer = mid
+        if total > K:
+            end = mid - 1
 
-res = 0
-while start <= end:
-    mid = (start + end) // 2
-    
-    hap = 0
-    for level in levels:
-        if mid > level:
-           hap+= (mid - level)
-           
-    if hap <= k:
-        start = mid+1
-        res = max(mid,res)
-    else:
-        end = mid -1
+# 입력
+N, K = map(int, input().split())
+level = [int(input()) for i in range(N)]
+level.sort()
+answer = 0
 
-
-print(res)
+# 출력 (목표레벨 T 값은?)
+game(level)
+print(answer)
