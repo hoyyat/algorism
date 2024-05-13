8d9e20ae00763fac6650c70539145c6958a3d014
JINHO KWACK <86460176+hoyyat@users.noreply.github.com>
2024-05-13 23:08:36 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,4 +1,3 @@
 function solution(N) {
-    const arr = N.toString().split('')
-    return arr.map(Number).reduce((acc, cur) => acc + cur, 0);
+    return (N+"").split("").map(Number).reduce((acc, cur) => acc + cur, 0);
 }
