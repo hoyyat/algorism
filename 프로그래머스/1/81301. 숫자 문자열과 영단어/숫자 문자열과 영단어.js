a596e307af7fde7ef837554a5d91d83ba4e99abb
JINHO KWACK <86460176+hoyyat@users.noreply.github.com>
2024-05-22 00:18:04 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,28 +1,25 @@
 function solution(s) {
     var answer = '';
     const number = {
-        0:'zero',
-        1:'one',
-        2:'two',
-        3:'three',
-        4:'four',
-        5:'five',
-        6:'six',
-        7:'seven',
-        8:'eight',
-        9:'nine',
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9
     }
     let temp = ''
     
     for (let i of s) {       
         if (isNaN(i)) {
             temp += i;
-            if (Object.values(number).includes(temp)) {
-                let key = Object.keys(number).find(key => number[key] === temp);
-                if (key) {
-                    answer += key;
-                    temp = '';
-                }
+            if (temp in number) {
+                answer += number[temp];
+                temp = '';
             }
         } else {
             answer += i;
