768748899b91e234e474d8466c1c6fcb4a520bbf
JINHO KWACK <86460176+hoyyat@users.noreply.github.com>
2024-06-02 20:17:13 +0900

    [level 1] Title: 과일 장수, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,8 +1,12 @@
 function solution(k, m, score) {
-    let result = []
-    score.sort((a, b) => b - a)
+    // 점수 내림차순 정렬
+    score.sort((a, b) => b - a);
+    
+    // 누적 결과 저장할 변수 선언
+    let total = 0;
+    
     for (let i = 1; i <= score.length; i++) {
-        if(i % m === 0) result.push(score[i-1] * m)
+        if(i % m === 0) total += score[i-1] * m;
     }
-    return result.reduce((acc, cur) => acc + cur, 0)
+    return total;
 }
