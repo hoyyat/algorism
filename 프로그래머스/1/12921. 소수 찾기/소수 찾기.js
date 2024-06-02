db7473fd14ce7853e12fbfe6fc97faa6dee050c9
JINHO KWACK <86460176+hoyyat@users.noreply.github.com>
2024-06-02 22:50:59 +0900

    [level 1] Title: 소수 찾기, Time: 98.58 ms, Memory: 36.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,19 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
+}
+
+function solution(n) {
+    let count = 0;
+    for (let i = 2; i <= n; i++) {
+        if (isPrime(i)) {
+            count++;
+        }
+    }
+    return count;
+}
