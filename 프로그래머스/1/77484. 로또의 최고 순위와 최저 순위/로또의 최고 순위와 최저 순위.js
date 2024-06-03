3b3240f8ae8d435cf4666aa38138a9f9afb256fc
JINHO KWACK <86460176+hoyyat@users.noreply.github.com>
2024-06-03 19:39:29 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,5 +1,4 @@
 function solution(lottos, win_nums) {
-    let result = [0, 0]
     const rate = {
         '6': 1,
         '5': 2,
@@ -13,8 +12,7 @@ function solution(lottos, win_nums) {
     let winResult = win_nums.filter(v => lottos.includes(v)).length + lottos.filter(v => v === 0).length
     let roseResult = win_nums.filter(v => lottos.includes(v)).length
     
-    result[0] += rate[winResult];
-    result[1] = rate[roseResult];
+    let result = [rate[winResult], rate[roseResult]]
 
     return result;
 }
